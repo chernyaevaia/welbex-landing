a21779d82a9a60bedc0b0456970a44db265c3bbd
Ирина Черняева <iridache@192.168.1.7>
2023-03-13 11:05:27 +0300

    desktop done

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -9,8 +9,8 @@ export function Hero() {
       <div className={styles.wrapper}>
         <div>
           <h1 className={styles.header}>
-            Зарабатывайте <br /> больше <br />{" "}
-            <span className={styles.highlighted}>с WELBEX</span>{" "}
+            Зарабатывайте <br /> больше <br />
+            <span className={styles.highlighted}>с WELBEX</span>
           </h1>
           <div className={styles.subheading}>
             Развиваем и контролируем продажи за вас
